b89cd7743dfe49daa100325c74cfcb670a90e1c0
Deilon <deilast80@gmail.com>
2023-04-13 03:03:19 +0800

    UI: Update Nav and subnav

--- resources/views/Layouts/top.blade.php
+++ resources/views/Layouts/top.blade.php
@@ -5,32 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">   
-    <link rel="stylesheet" href="{{ asset('styles/main.css') }}">
-</head>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
 <body>
     
-    <nav class="navbar navbar-expand-lg navbar-light border-bottom">
-        <div class="container">
-          <a class="navbar-brand fw-bold" href="/feed">AIM Corner</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav, #navbarSubNav, #navbarSubNav2" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/feed/all/posts">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/profile">Profile</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-danger" href="/logout">Logout</a>
-              </li>
-            </ul>
-            <a href="/add/post" class="btn btn-light" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add Post"><i class="bi bi-plus-lg"></i></a>
+<nav class="bg-white">
+  <div class="relative container mx-auto px-6 py-5">
+
+    <div class="flex justify-between items-center">
+      <div class="flex space-x-5 items-center">
+        <a href="#" class="mr-5"><img src="http://localhost:8000/images/logo-dark.svg" alt="logo" width="60"></a>
+        <a href="http://" class="border-b-2 border-black py-2">Home</a>
+        <a href="http://">Profile</a>
+        <a href="http://">Dashboard</a>
+        <a href="http://">Control panel</a>
+      </div>
+
+      <div class="flex items-center">
+        <button class="mr-5"><svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16"><path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/></svg></button>
+        <div class="flex items-center p-2 hover:bg-gray-200 hover:cursor-pointer">
+          <div class="flex items-center" style="width: 200px !important">
+            <img src="{{ asset('images/user_photo.png') }}" width="33" height="33" class="mr-3" alt="default photo">
+            <div class="text-ellipsis overflow-hidden">student777</div>  
           </div>
-        </div>
-    </nav>
+            <i class="bi bi-caret-down"></i>
+        </div>    
+      </div>
+    </div>
+
+
+  </div>
+</nav>
--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,148 +1,17 @@
 @include('Layouts.top')
 
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-          <div class="collapse navbar-collapse" id="navbarSubNav">
-            <ul class="navbar-nav interests-nav d-flex flex-row flex-wrap justify-content-evenly">
-              <li class="">
-                <a class="nav-link" aria-current="page" href="#">For You</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active-interests-nav" href="#">All</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Mathematics</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Technology</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Social</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Literature</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Science</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Psychology</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-          <div class="collapse navbar-collapse" id="navbarSubNav2">
-            <ul class="navbar-nav d-flex flex-row flex-wrap justify-content-evenly">
-              <li class="nav-item">
-                <a class="nav-link active" id="gridView" aria-current="page" style="cursor: pointer"><i class="bi bi-grid-fill"></i> Grid View</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" id="listView" aria-current="page" style="cursor: pointer"><i class="bi bi-justify"></i> List View</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ url('feed/admin/posts') }}"><i class="bi bi-square-fill" style="color: #E6E4FF"></i> College Coordinator</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ url('feed/instructor/posts') }}"><i class="bi bi-square-fill" style="color: #FFE9E9"></i> College Instructors</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ url('feed/student/posts') }}"><i class="bi bi-square-fill" style="color: #CDF6BC"></i> Students</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-
-    <section class="mt-4">
-        <div class="container">
-            <div class="row"> <!-- add data-masonry='{"percentPosition": true }' for masonry effect but bug will occur in list view -->
-
-              @foreach ($posts as $post)
-
-                <div class="post-card col-md-6 col-lg-6 mb-4">
-
-                  @if($post->type == "photo")
-                    <div class="post-img" style="background-image: url('/images/post_photo.png');">
-                        <div class="post-backdrop-img"></div>
-                    </div>
-                  @endif
-
-                  @if ($post->user->role == "admin")
-                    <div class="light-blue p-4 clearfix" style="border-radius: 8px" onclick="location.href='/view/post/{{$post->id}}'">  
-                  @elseif ($post->user->role == "instructor")
-                    <div class="light-red p-4 clearfix" style="border-radius: 8px" onclick="location.href='/view/post/{{$post->id}}'">
-                  @elseif ($post->user->role == "student")
-                    <div class="light-green p-4 clearfix" style="border-radius: 8px" onclick="location.href='/view/post/{{$post->id}}'">
-                  @endif
-                  
-
-                      <div class="d-flex" style="color: #828080; font-family: 'Raleway', sans-serif; font-size: 500;">
-                          <p>Posted by <span class="text-decoration-underline">{{$post->user->firstname}} {{$post->user->lastname}} ( {{$post->user->role}} )</span></p>
-                          <span class="ms-auto" style="color: #828080; font-family: 'Segoe UI', sans-serif;">13h ago</span>
-                      </div>
-
-                      <!-- post content -->
-                      <div class="post-content bg-white p-3" style="--bs-bg-opacity: .38; border-radius: 8px; height: 180px;">
-                          
-                          <!-- Vote button -->
-                          <div class="d-flex flex-column float-start me-4">
-                              <button style="background: transparent; border: none;">
-                                  <i class="bi bi-caret-up" style="font-size: 2rem; color: #707070;"></i>
-                              </button>
-                              <p class="vote-count text-center">445</p>
-                              <button style="background: transparent; border: none;">
-                                  <i class="bi bi-caret-down" style="font-size: 2rem; color: #707070;"></i>
-                              </button>              
-                          </div>
-
-                          <!-- post text/img/link -->
-                          <div>
-                            @if ($post->type == "title")
-                              <p style="font-size: 39px;">
-                                {{$post->title}}
-                              </p>
-                              {{$post->text}}  
-                            @elseif ($post->type == "photo")
-                              <p class="mt-2" style="font-size: 17px;">
-                                {{$post->title}}
-                              </p>
-                            @elseif ($post->type == "link")
-                              <p class="mt-2" style="font-size: 17px;">
-                                {{$post->title}}
-                              </p>
-                              <a href="{{$post->link}}">{{$post->link}}</a>
-                            @endif
-                          </div>
-
-                      </div>
-
-                      <div class="d-flex mt-4">
-                          <button class="post-metrics-btn me-2"><i class="bi bi-chat-left-dots" style="font-size: 1rem"></i> {{count($post->comment)}} Comments</button>
-                          <button class="post-metrics-btn"><i class="bi bi-bookmark" style="font-size: 1rem"></i> Save</button>
-                      </div>
-
-                  </div>
-                </div>
-              @endforeach
-
-            </div>
-        </div>
-        </div>
-    </section> 
+<section id="feed-nav" class="mt-5">
+  <div class="container mx-auto px-6">
+    <div class="flex items-center space-x-10 bg-white border border-slate-300 rounded-md px-6">
+      <a href="#" class="py-5 pe-2 border-b-2 border-black"><i class="bi bi-house-fill mr-2"></i> For you</a>
+      <a href="#" class="py-5 pe-2"><i class="bi bi-asterisk mr-2"></i> Show all</a>
+      <a href="#" class="py-5 pe-2"><i class="bi bi-people-fill mr-2"></i> Following</a>
+      <a href="#" class="py-5 pe-2"><i class="bi bi-mortarboard-fill mr-2"></i> Students</a>
+      <a href="#" class="py-5 pe-2"><i class="bi bi-person-fill mr-2"></i> Instructors</a>
+      <a href="#" class="py-5 pe-2"><i class="bi bi-person-badge-fill mr-2"></i> Administrators</a>
+    </div>
+  </div>
+</section>
     
 @include('scripts')
-<script>
-      $( "#listView" ).click(function() {
-        $('.post-card').addClass('offset-lg-3 offset-md-3');
-      });      
-      $( "#gridView" ).click(function() {
-        $('.post-card').removeClass("offset-lg-3 offset-md-3");
-      });
-</script>
-
 @include('Layouts.bottom')
