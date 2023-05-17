4d4623ebb53cc803d8e945e6a2b631d96b6d5c9a
Deilon <deilast80@gmail.com>
2023-05-17 11:47:24 +0800

    Add account toggle

--- resources/views/Layouts/bottom.blade.php
+++ resources/views/Layouts/bottom.blade.php
@@ -11,6 +11,20 @@
         </div>
     </footer>
     
-<script src="{{ asset('js/main.js')}}"></script>
+<script>
+    // Account toggle
+    $(function() {
+        $('#acct-btn').click(function(e) {
+            e.stopPropagation();
+            $('.acct-dropdown-content').toggle()
+        });
+
+        $(document).on('click', function(e) {
+            if (!$('.acct-dropdown-content').is(e.target) && $('.acct-dropdown-content').has(e.target).length === 0) {
+                $('.acct-dropdown-content').hide();
+            }
+        });
+    });
+</script>
 </body>
 </html>
--- resources/views/Layouts/top.blade.php
+++ resources/views/Layouts/top.blade.php
@@ -25,14 +25,10 @@
     <div class="flex justify-between items-center">
       <div class="flex space-x-5 items-center">
         <a href="{{ url('feed/all') }}" class="mr-5"><img src="http://localhost:8000/images/logo-dark.svg" alt="logo" width="60"></a>
-        <a href="{{ url('feed/all') }}" class="border-b-2 border-black py-2">Home</a>
-        <a href="{{ url('profile') }}">Profile</a>
-        <a href="{{ url('dashboard/posts') }}">Dashboard</a>
-        <a href="{{ url('control-panel') }}">Control panel</a>
       </div>
 
       <div class="flex items-center space-x-2">
-        <button class="mr-5"><svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16"><path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/></svg></button>
+        <button class="mr-5 hover:cursor-not-allowed hover:text-gray-500"><svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16"><path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/></svg></button>
         <a href="{{ url('add/post') }}" class="py-1 px-2 rounded-sm bg-slate-200 hover:bg-slate-300"><i class="bi bi-plus-lg"></i></a>
         <div class="relative">
           <button id="acct-btn" class="flex items-center p-2 hover:bg-gray-200 hover:cursor-pointer">
@@ -47,6 +43,9 @@
               <i class="bi bi-caret-down"></i>
           </button>
           <div class="absolute hidden flex flex-col shadow-2xl bg-white border border-slate-300 py-5 rounded w-full acct-dropdown-content z-40">
+            <a href="{{ url('profile') }}" class="block p-3 bg-gray-100 hover:bg-gray-200 hover:cursor-pointer">Profile</a>
+            <a href="{{ url('dashboard/posts') }}" class="block p-3 bg-gray-100 hover:bg-gray-200 hover:cursor-pointer">Dashboard</a>
+            <a href="#" class="disabled-link block p-3 bg-gray-100 hover:cursor-not-allowed hover:text-gray-500">Control panel</a>
             <a href="/logout" class="block p-3 bg-gray-100 hover:bg-gray-200 hover:cursor-pointer">Sign out</a>
           </div>
         </div>
